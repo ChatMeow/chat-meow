ab7388c0b07bad3c5a3f7244eb9adaed59f00f0d
MeowKJ <ijink@qq.com>
2023-02-06 19:44:47 +0800

    删除多余的encoding参数

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 Author: MeowKJ
 Date: 2023-01-25 00:57:53
 LastEditors: MeowKJ ijink@qq.com
-LastEditTime: 2023-02-06 01:32:44
-FilePath: /chat-meow-core/main.py
+LastEditTime: 2023-02-06 19:43:28
+FilePath: /chat-meow/main.py
 '''
 from threading import Thread
 from meow.web.app import create_app
@@ -12,7 +12,7 @@ import logging
 
 if __name__ == '__main__':
     logging.basicConfig(
-        level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s', encoding='utf-8')
+        level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
     
     chat_thread = Thread(target=chat_loop, name='chat_thread')
     chat_thread.setDaemon(True)
