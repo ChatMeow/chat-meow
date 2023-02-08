25e70c41e945bc94d91e1e384780342f08434a16
MeowKJ <ijink@qq.com>
2023-02-08 17:01:55 +0800

    滤波

--- meow/audio/record.py
+++ meow/audio/record.py
@@ -2,7 +2,7 @@
 Author: MeowKJ
 Date: 2023-01-25 14:25:18
 LastEditors: MeowKJ ijink@qq.com
-LastEditTime: 2023-02-08 16:54:01
+LastEditTime: 2023-02-08 16:59:36
 FilePath: /chat-meow/meow/audio/record.py
 '''
 import audioop
@@ -50,11 +50,15 @@ class RecordHandler(object):
             if (get_chat_thread_stop_flag()):
                 logging.info('STOP THE THREAD because ThreadStopException')
                 raise (ThreadStopException)
+            
             detect_count += 1
 
             stream_data = stream.read(self.chunk)
 
             rms = audioop.rms(stream_data, 2)
+            if(detect_count < 10):
+                continue
+
             logging.debug('RECORD  ' + str(rms))
             if rms > self.audio_min_rms:
                 high_audio_flag = high_audio_flag + 1
