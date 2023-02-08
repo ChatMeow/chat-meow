3e325e8b4dde8a8876a1ee968dd599039e7c75ea
MeowKJ <ijink@qq.com>
2023-02-08 17:15:10 +0800

    优化

--- meow/audio/record.py
+++ meow/audio/record.py
@@ -2,7 +2,7 @@
 Author: MeowKJ
 Date: 2023-01-25 14:25:18
 LastEditors: MeowKJ ijink@qq.com
-LastEditTime: 2023-02-08 17:11:25
+LastEditTime: 2023-02-08 17:14:31
 FilePath: /chat-meow/meow/audio/record.py
 '''
 import audioop
@@ -33,6 +33,7 @@ class RecordHandler(object):
         self.audio_frames = []
 
     def detect_audio(self):
+        self.play_stream.close()
         stream = self.pyaudio_instance.open(format=self.stream_format,
                                             channels=self.channels,
                                             rate=self.rate,
@@ -51,13 +52,13 @@ class RecordHandler(object):
             if (get_chat_thread_stop_flag()):
                 logging.info('STOP THE THREAD because ThreadStopException')
                 raise (ThreadStopException)
-            
+
             detect_count += 1
 
             stream_data = stream.read(self.chunk)
 
             rms = audioop.rms(stream_data, 2)
-            if(detect_count < 10):
+            if (detect_count < 10):
                 continue
 
             logging.debug('RECORD  ' + str(rms))
@@ -83,29 +84,30 @@ class RecordHandler(object):
         stream.close()
         txt = b''.join(self.audio_frames)
         self.audio_frames = []
+        self.before_play_from_str()
         return 0, txt
 
     def terminate(self):
         self.pyaudio_instance.terminate()
 
+    # 似乎在目标设备直接播放会丢失一小段，先打开设备一会儿
+
+    def before_play_from_str(self) -> None:
+        self.play_stream = self.pyaudio_instance.open(format=8, channels=1,
+                                                      rate=16000, output=True)
 
     def play_from_str(self, audio_txt: str) -> None:
-
-        stream = self.pyaudio_instance.open(format=8, channels=1,
-                        rate=16000, output=True)
-        time.sleep(1)
-        stream.write(audio_txt)
-        stream.stop_stream()
-        stream.close()
-
+        self.play_stream.write(audio_txt)
+        self.play_stream.stop_stream()
+        self.play_stream.close()
 
     def play_from_wav(self, audio_wav: str) -> None:
         chunk = 1024
         f = wave.open(audio_wav, "rb")
         stream = self.pyaudio_instance.open(format=self.pyaudio_instance.get_format_from_width(f.getsampwidth()),
-                        channels=f.getnchannels(),
-                        rate=f.getframerate(),
-                        output=True)
+                                            channels=f.getnchannels(),
+                                            rate=f.getframerate(),
+                                            output=True)
         data = f.readframes(chunk)
         while data != '':
             stream.write(data)
